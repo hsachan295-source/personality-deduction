d6575d934460caceabf2c551b4f41a788ba6ab09
Harsh Sachan <hsachan295@gmail.com>
2026-04-27 18:58:34 +0530

    Import Plotly for data visualization
    Added Plotly imports for enhanced visualizations.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-
 import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-
-
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
 
 # ─────────────────────────────────────────────
 #  PAGE CONFIG
